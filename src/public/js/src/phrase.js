b460e42583db5d73c325bc8e84ee1f85a08d3589
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2015-10-14 16:23:12 -0400

    front end

--- src/public/js/src/phrase.js
+++ src/public/js/src/phrase.js
@@ -1,7 +1,7 @@
 function Phrase(data){
 	this.data = data;
 	this.early = ((this.data.time_string == 'very early' || this.data.time_string == 'early') ? true : false);
-	this.out_late = this.early && this.data.previous_time_string == 'very late';
+	this.out_late = this.early && this.data.previous_time_string == 'very late' && !this.data.home;
 	this.time_since = parseDifference(this.data.date, this.data.previous_session_date)
 	this.first_session = this.out_late || this.early && this.time_since.hours > 7;
 	this.has_moved = this.data.city != this.data.previous_session_city;
@@ -13,6 +13,10 @@ function Phrase(data){
 
 }
 var buildSentence = function(data){
+// frequent checker and at work
+//cool side of the pillow
+// not home not at work in the afternoon
+
 	if (data.early && data.out_late && data.first_session){
 		data.sentence = new Sentence(data).hungover();		
 	}else if (data.data.time_string == 'very late'){
@@ -27,10 +31,12 @@ var buildSentence = function(data){
 		data.sentence = new Sentence(data).hasMoved();
 	}else if(data.data.work == true){
 		data.sentence = new Sentence(data).work();
-	}else if(data.data.time_string == 'evening' || data.data.time_string == 'night' && data.data.work == true){
+	}else if(data.data.time_string == 'evening' && data.data.work == true){
 		data.sentence = new Sentence(data).workLate();
-	}else if(data.time_since.hours < 2 && !data.has_moved){
+	}else if(data.time_since.hours <= 1 && !data.has_moved){
 		data.sentence = new Sentence(data).frequentChecker();	
+	}else if(data.early && !data.has_moved){
+		data.sentence = new Sentence(data).earlyNotMoved();	
 	}
 	else{
 		data.sentence = new Sentence(data).defaultSentence();
--- src/public/js/src/sentence.js
+++ src/public/js/src/sentence.js
@@ -4,70 +4,75 @@ function Sentence(data){
 }
 Sentence.prototype.hungover = function(){
 	var str = '';
-	str = str + "It's " + this.data.data.date.replace('EDTIP', '')+", "+this.data.data.time_string+' considering how late you were out last night.';	
-	str = str + " You drank so much last night in "+this.data.data.city + ' that you have to look at a wall to keep the room from spinning. You feel like did something stupid, but you cant remember what.'
-	str = str + " You hope you weren't tagged in any of their photographs."
+	str = str + "It's " + this.data.data.date.replace('EDTIP', '')+", you are up "+this.data.data.time_string+' considering how late you were out last night. ';	
+	str = str + this.data.data.city + ' must have been fun, but I bet you are hung over.  Why dont you tell me whats on your mind?'; 
 	this.advertisement = 'seltzer';
 	return str;
 };
 Sentence.prototype.cantSleep = function(){
-	var option1 = '';
-	var option2 = '';
 	var str = '';
 	if(this.data.data.home){
-		var ar = [option1, option2];
-		option1 = option1 + "It's late, you should be asleep but you check me one more time just to be sure. The date is" + this.data.data.date.replace('EDTIP', '') + '.';
-		option2 = option2 + "You had that dream again and now you are awake. It's " + this.data.data.date.replace('EDTIP', '') + ' and now you are checking me because my light comforts you.';
+		var ar = [
+			"It's late, you should be asleep but you check me one more time just to be sure. The date is " + this.data.data.date.replace('EDTIP', '') + ' and you are at home in '+ this.data.data.city + '.',
+		 	"You had that dream again and now you are awake. It's " + this.data.data.date.replace('EDTIP', '') + ' and you are checking me to forget.',
+		 	"You are at home and can't sleep. The date is " + this.data.data.date.replace('EDTIP', '') +  " and you are looking for picture of yourself when you were young, again."
+		 	]
 		
 		this.advertisement = 'sleep aid';
-		
+
 		return ar[Math.floor(Math.random() * ar.length)];
 	}else{
-		str = str + "You are out late with your friends in " + this.data.data.city + ".  You were having a great time until your friend's left." 
-		str = str +" Now you are at the bar alone pretending to be reading a work email while actually checking me for updates about your ex." ;
+		var ar = [
+		"You were having a great time with your friends in " + this.data.data.city + " on  "+ this.data.data.date.replace('EDTIP', '') +  " but they all went home. Now you are at the bar alone pretending to be reading a work email while actually checking me for no particular reason.",
+		"You are in " + this.data.data.city + " and it's  "+ this.data.data.date.replace('EDTIP', '') +  ". You don't know how you got here and you wonder why you are on facebook. Tell me what's on your mind.",
+		"It's  "+ this.data.data.date.replace('EDTIP', '') +  ". You had that dream again and when you woke up your shoes were on. You had logged in to facebook from " + this.data.data.city + ", not the same place you fell asleep."
+		]
 		this.advertisement = 'okay cupid';
-		return str;
+		return ar[Math.floor(Math.random() * ar.length)];
 	}
 	
 }
 Sentence.prototype.frequentChecker = function(){
 	
-	var str = "You checked me " + this.data.time_since.hours +" hours and " + this.data.time_since.minutes + " ago."
-	str = str + " A lot has changed, and I can't wait for you to check me again."
+	var ar = [
+	"You checked me " + this.data.time_since.hours +" hours and " + this.data.time_since.minutes + " minutes ago. A lot has changed, and I can't wait for you to check me again.",
+	"You are still in " + this.data.data.city + ". As you closed your facebook window "+ this.data.time_since.hours +" hours and " + this.data.time_since.minutes + " minutes ago you thought you saw a photo of yourself. You log back in and can't find the picture.",
+	"When you last checked me, "+ this.data.time_since.hours +" hours and " + this.data.time_since.minutes + " minutes ago, you noticed that someone you used to know from high school passed away.  You log back in to find out how."
+	]
 	
 	this.advertisement = 'frequent checker';
-	return str;
+	return ar[Math.floor(Math.random() * ar.length)];
+
 }	
 Sentence.prototype.work = function(){
 	
 
-	var str = "You are bored at work on " + this.data.data.date.replace('EDTIP', '') + ".";
-	str = str + " You resize your a new browser window, so that it is small enough not to be seen by those around you.";
-	str = str + "As you start typing 'faceb', you think to yourself, 'tomorrow I am going to look for a new job'.";
+	var ar = [
+	"You are bored at work on " + this.data.data.date.replace('EDTIP', '') + ". You resize your a new browser window, so that it is small enough not to be seen by those around you. As you start typing 'faceb', you think to yourself, 'tomorrow I am going to look for a new job'.",
+	"On "+this.data.data.date.replace('EDTIP', '') +" you look your boss up on facebook.  In her profile picture she is holding a cocktail, but you could have sworn she said she was sober.",
+	"On your work computer "+this.data.data.date.replace('EDTIP', '') +  ", you accidently return to facebook, after typing f into google chrome.",
+	"You are at work and you want to quit. It's "+this.data.data.date.replace('EDTIP', '') +  ", you march over to your boss and ask if she want's to chat over a drink after work.  She says she is sober.  You log into facebook and consider removing every photo of yourself."
+	]
 	this.advertisement = 'linkedin';
-	return str;
-
-
-
+	return ar[Math.floor(Math.random() * ar.length)];
 
 }
 Sentence.prototype.workLate = function(){
-	var str = "You at work late on " + this.data.data.date.replace('EDTIP', '') + ".";
-	str = str + " You are hungry and tired. Your boss just doesn't pay enough attention to all the work that you are putting in."
-	str = str + " You worked hard for a promotion, but you just can't help but feel like your life isn't going anywhere."
-	str = str + "As you start typing 'faceb', you think to yourself, 'tomorrow I am going to look for a new job'.";
+	var ar =[
+	"You at work late on " + this.data.data.date.replace('EDTIP', '') + ". You are hungry and tired. Your boss just doesn't pay enough attention to all the work that you are putting in and you can't help but feel like your life isn't going anywhere. As you start typing 'faceb', you think to yourself, 'tomorrow I am going to look for a new job'.",
+	"You are at work late again on " + this.data.data.date.replace('EDTIP', '') +". You hear somone shuffling around in the hallway.  When you peer over your partition to see who it is, you find your boss sleeping on the floor.  You check me for updates and find out that the identical twin of someone you used to know from high school has passed away."]
 	this.advertisement = 'linkedin';
-	return str;
+	return ar[Math.floor(Math.random() * ar.length)];
 }
 Sentence.prototype.wakeup = function(place){
 	var str = '';
 	if (place){
-		str = str + " On " + this.data.data.date.replace('EDTIP', '')+ ", you wake up. You roll over onto the cool side of the pillow and check me for updates.";
+		str = str + " On " + this.data.data.date.replace('EDTIP', '')+ ", you wake up and roll over onto the cool side of the pillow to check me for updates.";
 		this.advertisement = 'cereal';
 	}else{
 		str = str + " On" + this.data.data.date.replace('EDTIP', '') + " you wake up in a bed that isn't yours in " +place+ ".";
 		if (this.data.data.frequency_string == 'once'){
-			str = str + " Another one night stand.  You are an adult, maybe it's time you settle down."
+			str = str + " Another one night stand. You are conflicted as to whether you update your status or not."
 			this.advertisement = 'condoms';
 		}		 
 	}
@@ -81,14 +86,23 @@ Sentence.prototype.hasMoved = function(){
 		this.advertisement = 'car';
 	}else if (this.data.data.previous_work && this.data.data.home){
 		str = str + ' You come home from work on '+ this.data.data.date.replace('EDTIP', '') + '.';
-		str = str + ' Another day that felt like the last - you might as well check me to see some updates.';
+		str = str + ' Every day is starting to feel the same- you check me to see if you forgot to wish someone a happy birthday.';
 		this.advertisement = 'car';
 	}else{
 		str = str + "It's " + this.data.data.date.replace('EDTIP', '')+", in the "+ this.data.time_since.hours +" hours and " +this.data.time_since.minutes + " minutes that you were away from me you have moved from " + this.data.data.previous_session_city + " to " + this.data.data.city+'.';
 	}
 	return str;
 }
+Sentence.prototype.earlyNotMoved = function(){
+	var ar =[ 
+		"You are still in bed in " + this.data.data.previous_session_city + ". It's been" + this.data.time_since.hours + " hours and " +this.data.time_since.minutes + " minutes, since I last heard from you. What's on your mind?",
+		this.data.data.date.replace('EDTIP', '') + ", you checked me "  + this.data.time_since.hours + " hours and " +this.data.time_since.minutes + " minutes ago.",
+		"After you checked me "  + this.data.time_since.hours + " hours and " +this.data.time_since.minutes + " minutes ago, you fell asleep while reaching out to an old friend named Molly.  You log in to me and discover that you don't have a friend named Molly."]
+		
+		return ar[Math.floor(Math.random() * ar.length)];
+}
 Sentence.prototype.defaultSentence = function(){
 	var str = 'default';
+debugger;
 	return str;
 }
